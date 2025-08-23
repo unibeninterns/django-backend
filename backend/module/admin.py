28bfb96b1583f6c51d292863127221a32b0e801f
Umunu Odafe Peter <odafeumunu@gmail.com>
2025-08-23 23:21:02 +0100

    update frontend path

--- backend/module/admin.py
+++ backend/module/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Lesson)
+admin.site.register(ContentItem)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(QuizSubmission)
+admin.site.register(Answer)
+admin.site.register(Payment)
+admin.site.register(Enrollment)
+admin.site.register(CapstoneProject)
+admin.site.register(LiveSession)
+
+
+
+
+
+
+
+
+
+
