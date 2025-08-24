1d1c76b49676b03c093e867f6448976d939ad2f9
Umunu Odafe Peter <odafeumunu@gmail.com>
2025-08-24 01:01:25 +0100

    Saving local changes before pulling

--- backend/module/admin.py
+++ backend/module/admin.py
@@ -1,25 +1,3 @@
 from django.contrib import admin
-from .models import *
 
-admin.site.register(Course)
-admin.site.register(Module)
-admin.site.register(Lesson)
-admin.site.register(ContentItem)
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(QuizSubmission)
-admin.site.register(Answer)
-admin.site.register(Payment)
-admin.site.register(Enrollment)
-admin.site.register(CapstoneProject)
-admin.site.register(LiveSession)
-
-
-
-
-
-
-
-
-
-
+# Register your models here.
