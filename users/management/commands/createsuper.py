ddcdfadff06209c30f8bfb3ed3003d465e4e763d
Umunu Odafe Peter <odafeumunu@gmail.com>
2025-09-14 20:30:45 +0100

    add dotenv

--- users/management/commands/createsuper.py
+++ users/management/commands/createsuper.py
@@ -1,6 +1,9 @@
 import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from dotenv import load_dotenv
+
+load_dotenv()
 
 User = get_user_model()
 
